df3b0ffba351e9e586a17bc70e5c280e2f5fe58a
Frank420-Developer <panchoget300@gmail.com>
2021-10-27 11:48:48 -0500

    Initial Commit

---
+++ src/app/ui/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public titulo = 'Iniciar Sesión';
+  public saludoBienvenida: string;
+
+  constructor() {
+    console.log("Inicialización en el constructor");
+    this.saludoBienvenida = '';
+  }
+
+  /**
+   * @description Método implementado por parte de OnInit
+   */
+  ngOnInit(): void {
+    console.log("Inicialización en el OnInit");
+    console.log(sumarDosNumeros(12, 12));
+  }
+
+}
+
+function sumarDosNumeros(numeroUno: number, numeroDos: number){
+  return numeroUno + numeroDos;
+}
