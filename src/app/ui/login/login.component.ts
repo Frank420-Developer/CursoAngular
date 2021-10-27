86a0f38b2ccab1bec8a301215d4a8bc00ecba51a
Frank420-Developer <panchoget300@gmail.com>
2021-10-27 13:09:03 -0500

    third commit

--- src/app/ui/login/login.component.ts
+++ src/app/ui/login/login.component.ts
@@ -25,6 +25,21 @@ export class LoginComponent implements OnInit {
     console.log(sumarDosNumeros(12, 12));
   }
 
+  /**
+   * @description Método encargado de realizar la obtención de los datos del usuario
+   * @param usuario (string) Nombre de usuario del cliente. Es usado para validar que la cuenta existe
+   * @param contraseña (string) Contraseña de la cuenta. Es usada para validar que la cuenta es valida.
+   */
+  private obtenerUsuario(usuario: string, contraseña: string): void{
+    //funcionabilidad
+  }
+
+
+  public inicioSesion(): boolean{
+
+    return true;
+  }
+
 }
 
 function sumarDosNumeros(numeroUno: number, numeroDos: number){
